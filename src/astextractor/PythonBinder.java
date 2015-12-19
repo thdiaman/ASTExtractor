742cb53beaac883e9a07e407bd60b3b9f3fa3c95
thdiaman <themisdiamantopoulos@hotmail.com>
2015-12-19 11:21:09 +0200

    Added python bindings to the library

---
+++ src/astextractor/PythonBinder.java
@@ -0,0 +1,67 @@
+package astextractor;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Scanner;
+
+import javax.xml.bind.DatatypeConverter;
+
+/**
+ * Class used to bind this library to a python file.
+ * 
+ * @author themis
+ */
+public class PythonBinder {
+
+	/**
+	 * Function used to bind this library to a python file. It works by opening a {@link Scanner} on the standard input,
+	 * reading the required task in the form of a message and writing the result as a message in the standard output.
+	 * The base 64 format is used to send the messages.
+	 * 
+	 * @param args unused parameter
+	 * @throws IOException
+	 */
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		while (scanner.hasNextLine()) {
+			// Receive message and decode it
+			String b64message = scanner.nextLine();
+			String message = new String(DatatypeConverter.parseBase64Binary(b64message));
+
+			// Operate on message and return response
+			String messageresult = "";
+			if (message.equals("START_OF_TRANSMISSION")) {
+				messageresult = message;
+				String b64messageresult = DatatypeConverter
+						.printBase64Binary(messageresult.getBytes(Charset.forName("US-ASCII")));
+				System.out.println(b64messageresult);
+				System.out.flush();
+				continue;
+			} else if (message.equals("END_OF_TRANSMISSION")) {
+				messageresult = message;
+				String b64messageresult = DatatypeConverter
+						.printBase64Binary(messageresult.getBytes(Charset.forName("US-ASCII")));
+				System.out.println(b64messageresult);
+				System.out.flush();
+				break;
+			} else {
+				String messageHeader = message.split("_-_")[0];
+				message = message.substring(messageHeader.length() + 3);
+				String parseType = messageHeader.split("_")[1];
+				String parseRepr = messageHeader.split("_")[2];
+				if (parseType.equals("STRING"))
+					messageresult = ASTExtractor.parseString(message, parseRepr);
+				else if (parseType.equals("FILE"))
+					messageresult = ASTExtractor.parseFile(message, parseRepr);
+				else if (parseType.equals("FOLDER"))
+					messageresult = ASTExtractor.parseFolder(message, parseRepr);
+				String b64messageresult = DatatypeConverter
+						.printBase64Binary(messageresult.getBytes(Charset.forName("US-ASCII")));
+				System.out.println(b64messageresult);
+				System.out.flush();
+			}
+		}
+		scanner.close();
+	}
+
+}
---
+++ srcpy/astextractor.py
@@ -0,0 +1,123 @@
+import os
+import base64
+import subprocess
+from subprocess import STDOUT, PIPE
+
+class _ASTExtractor(object):
+	"""
+	Inner python binding to the ASTExtractor library. It works by executing the jar file as a subprocess
+	and opening pipes to the standard input and standard output so that messages can be sent and received.
+	Instead of using this class, it is highly recommended to use the abstracted ASTExtractor class.
+	"""
+	def __init__(self, path_to_ASTExtractor_jar):
+		"""
+		Initializes this inner extractor.
+		
+		:param path_to_ASTExtractor_jar: the path to the ASTExtractor jar.
+		"""
+		self.cmd = ['java', '-cp', path_to_ASTExtractor_jar, 'astextractor.PythonBinder']
+		self.proc = subprocess.Popen(self.cmd, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+		self.nummessages = 0
+		line = self.send_message("START_OF_TRANSMISSION")
+		if line != "START_OF_TRANSMISSION":
+			print("Error in Java compiler!!")
+			exit()
+
+	def close_extractor(self):
+		"""
+		Closes the extractor.
+		"""
+		return self.send_message("END_OF_TRANSMISSION") == "END_OF_TRANSMISSION"
+
+	def restart_extractor(self, force=False):
+		"""
+		Restarts the extractor.
+		"""
+		if force or self.send_message("END_OF_TRANSMISSION") == "END_OF_TRANSMISSION":
+			self.proc = subprocess.Popen(self.cmd, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+			self.nummessages = 0
+		else:
+			print("Error in Java compiler!!")
+			exit()
+
+	def get_ast(self, code_entity, code_entity_properties):
+		"""
+		Returns the AST of a code_entity.
+		
+		:param code_entity: the path or the contents of the code entity.
+		:param code_entity_properties: the properties of the code entity including its type and expected representation.
+		"""
+		self.nummessages += 1
+		if self.nummessages == 10000:
+			self.restart_extractor()
+		return self.send_message(code_entity_properties + code_entity)
+
+	def send_message(self, message):
+		"""
+		Sends a new message to the ASTExtractor jar.
+		
+		:param message: the message to be sent.
+		"""
+		decodedbytes = message.encode(encoding='ascii')
+		b64encodedbytes = base64.b64encode(decodedbytes)
+		self.proc.stdin.write(b64encodedbytes + b"\r\n")
+		self.proc.stdin.flush()
+		line = self.proc.stdout.readline()
+		try:
+			b64decodedbytes = base64.b64decode(line)
+			decodedline = b64decodedbytes.decode()
+		except:
+			self.restart_extractor(True)
+			decodedline = ""
+		return decodedline
+
+class ASTExtractor(_ASTExtractor):
+	"""
+	Class used as a python binding to the ASTExtractor library. It contains functions for parsing java code to AST.
+	"""
+	def __init__(self, path_to_ASTExtractor_jar):
+		"""
+		Initializes this AST Extractor.
+		
+		:param path_to_ASTExtractor_jar: the path to the ASTExtractor jar
+		"""
+		super(ASTExtractor, self).__init__(path_to_ASTExtractor_jar)
+
+	def parse_string(self, file_contents, representation="XML"):
+		"""
+		Parses the contents of a java file and returns its AST.
+
+		:param file_contents:  the contents of a java file, given as a string.
+		:param representation: the format of the returned AST, either "XML" or "JSON", default is "XML"..
+		:returns: a string containing the AST of the java file in XML or JSON format.
+		"""
+		return super(ASTExtractor, self).get_ast(file_contents, "PARSE_STRING_" + representation + "_-_")
+
+	def parse_file(self, filename, representation="XML"):
+		"""
+		Parses a java file and returns its AST.
+
+		:param filename: the filename of the java file to be parsed.
+		:param representation: the format of the returned AST, either "XML" or "JSON", default is "XML"..
+		:returns: a string containing the AST of the java file in XML or JSON format.
+		"""
+		filename = os.path.abspath(filename)
+		return super(ASTExtractor, self).get_ast(filename, "PARSE_FILE_" + representation + "_-_")
+
+	def parse_folder(self, folder_name, representation="XML"):
+		"""
+		Parses all the files of a folder and returns a unified AST.
+
+		:param folder_name: the path of the folder of which the files are parsed.
+		:param representation: the format of the returned AST, either "XML" or "JSON", default is "XML"..
+		:returns: an AST containing all the files of a folder in XML format.
+		"""
+		folder_name = os.path.abspath(folder_name)
+		return super(ASTExtractor, self).get_ast(folder_name, "PARSE_FOLDER_" + representation + "_-_")
+
+	def close(self):
+		"""
+		Closes the AST Extractor. Note that this function must be called after using the class.
+		Otherwise, this may result to a memory leak.
+		"""
+		super(ASTExtractor, self).close_extractor()
