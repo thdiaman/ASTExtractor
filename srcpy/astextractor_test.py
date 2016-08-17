a715f855b68b8dd9342fc53cbe19a5a0ba651e04
thdiaman <themisdiamantopoulos@hotmail.com>
2016-08-17 16:35:41 +0300

    Update python test jar version

--- srcpy/astextractor_test.py
+++ srcpy/astextractor_test.py
@@ -2,7 +2,7 @@ from astextractor import ASTExtractor
 
 if __name__ == '__main__':
 	'''Used as a test for the python bindings'''
-	ast_extractor = ASTExtractor("../target/ASTExtractor-0.2.jar", "../ASTExtractor.properties")
+	ast_extractor = ASTExtractor("../target/ASTExtractor-0.3.jar", "../ASTExtractor.properties")
 	ast = ast_extractor.parse_string(
 			"import org.myclassimports;\n" + 
 			"\n" + 
